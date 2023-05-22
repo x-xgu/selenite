683b7ab7101814d20797d6d49021727840faf0bc
xgu <g_xin@outlook.com>
2023-05-22 14:44:24 +0800

    NEW: mock

--- selenite/custom/ais/mock_ais.py
+++ selenite/custom/ais/mock_ais.py
@@ -1,78 +1,22 @@
 from typing import Any, Tuple
+
 from pyais import encode_dict
+
 from selenite.conf.protocol.socket import SocketConfig
 
 
 class ShipAIS:
-    """
-    Mock sending AIS messages to a socket.
-    """
-    mmsi: int = None
-    course: float = None
-    speed: float = None
-    position: Tuple[float, float] = None
-
-    def __init__(
-            self,
-            config: SocketConfig
-    ) -> None:
-        """
-        Initialize ShipAIS object with a SocketConfig object.
-
-        Args:
-            config (SocketConfig): SocketConfig object containing socket information.
-        """
+    def __init__(self, config: SocketConfig) -> None:
         self._socket = config
+        self.mmsi: int = 0
+        self.course: float = 0
+        self.speed: float = 0
+        self.position: Tuple[float, float] = (0, 0)
 
-    def encode_msg(
-            self
-    ) -> Any:
-        """
-        Encode AIS message into a byte string.
-
-        Returns:
-            Any: Encoded AIS message as a byte string.
-
-        Example:
-            >>> ais = ShipAIS(SocketConfig())
-            >>> ais.mmsi = 123456789
-            >>> ais.course = 90.5
-            >>> ais.speed = 10.2
-            >>> ais.position = (12.3456, 78.9101)
-            >>> ais.encode_msg()
-            b'!AIVDM,1,1,,A,13a:GQ0P0Qo=,0*5C'
-        """
+    def encode_msg(self) -> Any:
         lon, lat = self.position
-        msg = {
-            'mmsi': self.mmsi,
-            'type': 1,
-            'course': self.course,
-            'speed': self.speed,
-            'lon': lon,
-            'lat': lat
-        }
-        return encode_dict(
-            msg,
-            talker_id='AIVDM'
-        )[0]
+        msg = {'mmsi': self.mmsi, 'type': 1, 'course': self.course, 'speed': self.speed, 'lon': lon, 'lat': lat}
+        return encode_dict(msg, talker_id='AIVDM')[0]
 
-    def send_ais_msg(
-            self
-    ) -> None:
-        """
-        Send AIS message to the socket.
-
-        Returns:
-            None
-
-        Example:
-            >>> ais = ShipAIS(SocketConfig())
-            >>> ais.mmsi = 123456789
-            >>> ais.course = 90.5
-            >>> ais.speed = 10.2
-            >>> ais.position = (12.3456, 78.9101)
-            >>> ais.send_ais_msg()
-        """
-        self._socket.send_msg(
-            self.encode_msg()
-        )
+    def send_ais_msg(self) -> None:
+        self._socket.send_msg(self.encode_msg())
