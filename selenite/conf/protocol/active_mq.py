1e70702b8d2c97de0d8240d7619c6f76fcd88e8c
xgu <g_xin@outlook.com>
2023-05-29 15:07:22 +0800

    NEW: mq

--- selenite/conf/protocol/active_mq.py
+++ selenite/conf/protocol/active_mq.py
@@ -4,12 +4,19 @@ import stomp
 class MQConfig:
     activemq_ip: str = ''
     activemq_port: int = None
+    activemq_username: str = ''
+    activemq_password: str = ''
 
     def send_active_mq_msg(self, mq_topic, mq_msg):
         conn = stomp.Connection10(
             [(self.activemq_ip, self.activemq_port)],
             auto_content_length=False
         )
-        conn.connect()
+        conn.connect(
+            username=self.activemq_username,
+            passcode=self.activemq_password,
+            wait=True,
+            headers={'activemq.prefetchSize': '1'}
+        )
         conn.send(mq_topic, mq_msg)
         conn.disconnect()
