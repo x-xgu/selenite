4b52396367cf4660b5a4402b1b71af89f9982a5b
g_xin <g_xin@outlook.com>
2023-05-29 21:53:40 +0800

    fix: convert

--- selenite/common/convert.py
+++ selenite/common/convert.py
@@ -4,25 +4,40 @@ import cv2
 import numpy as np
 
 
-def zip_dict(keys: List[str], *values: List) -> List[Dict[str, str]]:
-    return [dict(zip(keys, value)) for value in values]
+def zip_dict(
+        keys: List[str],
+        *values: List
+) -> List[Dict[str, str]]:
+    return [
+        dict(zip(keys, value))
+        for value in values
+    ]
 
 
-def format_decimal_number_with_commas(number: float) -> str:
+def format_decimal_number_with_commas(
+        number: float
+) -> str:
     formatted_number = f'{float(number):,}'
     return formatted_number
 
 
-def bytes_to_numpy(bytes_data: bytes) -> np.ndarray:
+def bytes_to_numpy(
+        bytes_data: bytes
+) -> np.ndarray:
     nparr = np.frombuffer(bytes_data, np.uint8)
     return cv2.imdecode(nparr, cv2.IMREAD_GRAYSCALE)
 
 
-def convert_sec_to_ms(timeout: float) -> float:
+def convert_sec_to_ms(
+        timeout: float
+) -> float:
     return timeout * 1000
 
 
-def copy_object_without_attrs(obj: Any, *attrs: str) -> Any:
+def copy_object_without_attrs(
+        obj: Any,
+        *attrs: str
+) -> Any:
     return type(obj)(
         **{
             k: v
--- setup.py
+++ setup.py
@@ -1,12 +1,24 @@
 import os
+from typing import List
+
 from setuptools import setup, find_packages
 
 with open('README.md', 'r', encoding='utf-8') as fh:
     long_description = fh.read()
 
 
-def read_requirements(file_name):
-    with open(os.path.join(os.path.dirname(__file__), file_name)) as f:
+def read_requirements(
+        file_name: str
+) -> List:
+    """
+    Read requirements.txt file and return list of requirements.
+    """
+    with open(
+            os.path.join(
+                os.path.dirname(__file__),
+                file_name
+            )
+    ) as f:
         return f.read().splitlines()
 
 
