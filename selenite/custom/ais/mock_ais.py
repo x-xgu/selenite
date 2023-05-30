15fe51c592e8f2bcf1dbd4e0f426a1411998cda4
xgu <g_xin@outlook.com>
2023-05-30 09:23:11 +0800

    FIX: mock ais

--- selenite/custom/ais/mock_ais.py
+++ selenite/custom/ais/mock_ais.py
@@ -6,17 +6,44 @@ from selenite.conf.protocol.socket import SocketConfig
 
 
 class ShipAIS:
+    """
+    Mock AIS ship
+    """
     def __init__(self, config: SocketConfig) -> None:
-        self._socket = config
+        self._socket: SocketConfig = config
+
+        # MMIS: Maritime Mobile Service Identity
         self.mmsi: int = ...
+        # Name: Name of the vessel
+        self.name: str = ...
+        # Course: Course over ground in degrees
         self.course: float = ...
+        # Speed: Speed over ground in knots
         self.speed: float = ...
+        # Position: Position of the vessel
         self.position: Tuple[float, float] = ...
 
     def encode_msg(self) -> Any:
+        """
+        Encode AIS message
+        """
         lon, lat = self.position
-        msg = {'mmsi': self.mmsi, 'type': 1, 'course': self.course, 'speed': self.speed, 'lon': lon, 'lat': lat}
-        return encode_dict(msg, talker_id='AIVDM')[0]
+        msg = {
+            'mmsi': self.mmsi,
+            'shipname': self.name,
+            'type': 1,
+            'course': self.course,
+            'speed': self.speed,
+            'lon': lon,
+            'lat': lat
+        }
+        return encode_dict(
+            msg,
+            talker_id='AIVDM'
+        )[0]
 
     def send_ais_msg(self) -> None:
+        """
+        Send AIS message
+        """
         self._socket.send_msg(self.encode_msg())
