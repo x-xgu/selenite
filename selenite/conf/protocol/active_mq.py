2763ca9de1d56819e89543cbd04418a3780bb7a6
xgu <g_xin@outlook.com>
2023-05-30 10:30:34 +0800

    FIX: 注释

--- selenite/conf/protocol/active_mq.py
+++ selenite/conf/protocol/active_mq.py
@@ -2,12 +2,18 @@ import stomp
 
 
 class MQConfig:
+    """
+    ActiveMQ Config
+    """
     activemq_ip: str = ''
     activemq_port: int = None
     activemq_username: str = ''
     activemq_password: str = ''
 
-    def send_active_mq_msg(self, mq_topic, mq_msg):
+    def send_active_mq_msg(self, mq_topic: str, mq_msg: str) -> None:
+        """
+        Send ActiveMQ Message
+        """
         conn = stomp.Connection10(
             [(self.activemq_ip, self.activemq_port)],
             auto_content_length=False
--- selenite/conf/protocol/socket.py
+++ selenite/conf/protocol/socket.py
@@ -2,20 +2,37 @@ import socket
 
 
 class SocketConfig:
-    def __init__(self, *, ip: str, port: int) -> None:
+    """
+    SocketConfig is a class that contains the configuration for the socket
+    """
+    def __init__(
+            self,
+            *,
+            ip: str,
+            port: int
+    ) -> None:
         self.addr = (ip, port)
         self.socket = None
 
     def open_socket(self) -> None:
+        """
+        Open Socket
+        """
         self.socket = socket.socket(
             socket.AF_INET,
             socket.SOCK_DGRAM
         )
 
     def close_socket(self) -> None:
+        """
+        Close Socket
+        """
         self.socket.close()
 
     def send_msg(self, msg: str) -> None:
+        """
+        Send Message
+        """
         with socket.socket(
                 socket.AF_INET,
                 socket.SOCK_DGRAM
